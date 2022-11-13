0f48e28aba388c9c14c31cf020bf77c7da4c2d33
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-11-13 13:50:12 +0600

    updated for deploy issue

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors({origin:'http://localhost:3000'}));
+app.use(cors({origin:['http://localhost:3000', 'https://support-desk-app.onrender.com']}));
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
