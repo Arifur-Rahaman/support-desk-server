d96b0c06525328348645261877267c69a8cdf674
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-12-15 13:10:16 +0600

    updated

--- server.js
+++ server.js
@@ -14,13 +14,9 @@ app.use(cors({origin:['http://localhost:3000', 'https://support-desk-app.onrende
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-//* Serve static assets in production, must be at this location of this file.
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder
-    app.use(express.static('client/build'));
-    
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
+app.get('/', (req, res)=>{
+  res.json({message: 'Welcome to support desk api'})
+})
 
 app.use(errorHandler)
 
